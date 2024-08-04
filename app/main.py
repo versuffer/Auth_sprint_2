b197d6c40eab583a238c35658c57482694b69382
Яна Бубнова <bujhvh@mail.ru>
2024-08-04 08:30:08 +0300

    rate_limit_middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
+import datetime
 from pprint import pformat
 
+import redis
 import uvicorn
 from fastapi import FastAPI
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from app.api.api_router import api_router
 from app.core.config import app_settings
@@ -16,6 +21,36 @@ app = FastAPI(
 )
 
 app.include_router(api_router)
+
+redis_conn = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+
+@app.middleware("http")
+async def rate_limit_middleware(request: Request, call_next):
+    """Ограничение запросов от одного пользователя."""
+    token = request.headers.get("Authorization")
+    client_ip = request.client.host
+
+    identifier = token if token else client_ip
+
+    if not identifier:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"detail": "Missing Access Identifier"})
+
+    pipe = redis_conn.pipeline()
+    now = datetime.datetime.now()
+    key = f'{identifier}:{now.minute}'
+
+    pipe.incr(key, 1)
+    pipe.expire(key, 59)
+    result = pipe.execute()
+
+    request_number = result[0]
+    if request_number > app_settings.REQUEST_LIMIT_PER_MINUTE:
+        return JSONResponse(status_code=status.HTTP_429_TOO_MANY_REQUESTS, content={"detail": "Too Many Requests"})
+
+    response = await call_next(request)
+    return response
+
 
 if __name__ == '__main__':
     logger.info("Start with configuration: \n%s", pformat(app_settings.model_dump()))
